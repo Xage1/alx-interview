8908b6d89aff7976c6188d1c42fc1ac028578067
Xage1 <kennethkiarie555@gmail.com>
2024-05-02 09:22:12 +0000

    This

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -10,17 +10,16 @@ def makeChange(coins, total):
     """
     if total <= 0:
         return 0
+    check = 0
+    temp = 0
+    coins.sort(reverse=True)
+    for i in coins:
+        while check < total:
+            check += i
+            temp += 1
 
-    min_coins = [float('inf')] * (total + 1)
-    min_coins[0] = 0
-
-
-    for amount in range(1, total + 1):
-        for coin in coins:
-            if coin <= amount:
-                min_coins[amount] = min(min_coins[amount], min_coins[amount - coin] + 1)
-
-    if min_coins[total] == float('inf'):
-        return -1
-    else:
-        return min_coins[total]
+        if check == total:
+            return temp
+        check -= i
+        temp -= 1
+    return -1
