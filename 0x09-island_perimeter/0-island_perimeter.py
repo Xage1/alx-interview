c2d67fda7b314c7f9ab2b3b9083b8481fb0db457
Xage1 <kennethkiarie555@gmail.com>
2024-05-09 11:45:00 -0700

    This is awesome

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -4,20 +4,23 @@
 Perimeter grid task
 """
 
+
 def island_perimeter(grid):
     """
     Function to get the grid of a given array
     """
     perimeter = 0
 
-    directions = [(0,1), (0, -1), (1, 0 ), (-1, 0)]
+    directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 
     for i in range(len(grid)):
         for j in range(len(grid[0])):
             if grid[i][j] == 1:
                 for dx, dy in directions:
                     ni, nj = i + dx, j + dy
-                    if ni < 0 or ni >= len(grid) or nj < 0 or nj >= len(grid[0]) or grid[ni][nj] == 0:
+                    if ni < 0 or ni >= len(grid) \
+                            or nj < 0 or nj >= len(grid[0]) \
+                            or grid[ni][nj] == 0:
                         perimeter += 1
 
     return perimeter
