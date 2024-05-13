c599214f66fd823fde861b0a338624cdc5481e46
Xage1 <kennethkiarie555@gmail.com>
2024-05-13 08:20:16 -0700

    This is git

--- helper/db_operate.js
+++ helper/db_operate.js
@@ -10,3 +10,40 @@ async function createUser(userData) {
 		throw new Error('Failed to create user');
 	}
 }
+
+async function getUserById(userId) {
+	try {
+		const user = await User.findById(userId);
+		return user;
+	} catch (error) {
+		console.error(error);
+		throw new Error('Failed to get user');
+	}
+}
+
+async function updateUser(userId, userData) {
+	try {
+		const updateUser = await User.findByIdAndUpdate(userId, userData, { new: true });
+		return updatedUser;
+	} catch (error) {
+		console.error(error);
+		throw new Error('Failed to update user');
+	}
+}
+
+async function deleteUser(userId) {
+	try {
+		await User.findByIdAndDelete(userId);
+		return true;
+	} catch (error) {
+		console.error(error);
+		throw new Error('Failed to delete user');
+	}
+}
+
+module.exports = {
+	createUser,
+	getUserById,
+	updateUser,
+	deleteUser
+};
--- helper/server.js
+++ helper/server.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+	res.render('index');
+});
+
+
 app.listen(PORT, () => {
 	console.log(`Server is connected on port ${PORT}`);
 });
